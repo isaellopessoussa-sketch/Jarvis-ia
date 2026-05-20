39db5220f474c92cf970fa02439c899e521801aa
isaellopessoussa-sketch <isaellopessoussa@gmail.com>
2026-05-20 13:36:02 -0300

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -22,7 +22,7 @@ PROMPT_SISTEMA = (
 # Inicialização estável do modelo
 try:
     model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash", 
+        model_name="gemini pro", 
         system_instruction=PROMPT_SISTEMA
     )
 except Exception as e:
